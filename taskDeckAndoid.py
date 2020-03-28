66662d62d9fae935f8c801a06cfa175ef7ad6f0d
Marek Maskarinec <marekmaskarinec@gmail.com>
2020-03-28 17:06:00 +0100

    json config file support + bugfixes

--- taskDeckAndoid.py
+++ taskDeckAndoid.py
@@ -1,5 +1,6 @@
 import kivy
 import sqlite3
+import json
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
@@ -16,6 +17,7 @@ priorToSearch=10
 nowEditing=10
 seenNotes = list()
 lastId = 0
+index = 0
 
 #Builder.load_file('style.kv')
 
@@ -31,22 +33,39 @@ class MyGrid(Widget):
         self.aboutBt.bind(on_press=self.about)
         self.showAllBt.bind(on_press=self.showAll)
 
+        configFile = open("config.json", "r")
+        config = json.load(configFile)
+
+        self.addButton.text = config.get("addButton", "+")
+        self.buttonDid.text = config.get("doneButton", "DONE")
+        self.buttonLater.text = config.get("laterButton", "LATER")
+        self.showAllBt.text = config.get("showAllButton", "SHOW ALL")
+        self.aboutBt.text = config.get("aboutButton", "ABOUT")
+        self.settingsBt.text = config.get("settingsButton", "SETTINGS")
+
+
     def about(self, obj):
+        global index
         self.noteLabel.text = "By: Marek Maskarinec \n version: 0.1"
+        index = 0
 
     def showAll(self, obj):
+        global index
         conn = sqlite3.connect('cards.db')
         c = conn.cursor()
         c.execute("SELECT * FROM cards")
         cards = c.fetchall()
+        print(cards)
         if cards is not []:
             for i in range(len(cards)):
                 self.noteLabel.text += cards[i][0] + "\n" + str(cards[i][1]) + "\n" + "--------" + "\n"
         else:
             self.noteLabel.text = "No notes"
         conn.close()
+        index = 0
 
     def add(self, obj):
+        global index
         noteText = self.addTextBox.text
         priority = int(self.priorityBox.text )
         print(priority)
@@ -56,24 +75,48 @@ class MyGrid(Widget):
         #c.execute("""CREATE TABLE cards
         #            (note text, priority number)""")
         c.execute("INSERT INTO cards VALUES (?, ?)", (noteText, priority))
+        c.execute("SELECT * FROM cards ORDER BY priority DESC")
         conn.commit()
         conn.close()
+        index = 0
         #nowEditing = nowEditing - 1
 
     def didIt(self, obj):
+        global index
         global priorToSearch
         conn = sqlite3.connect('cards.db')
         c = conn.cursor()
-        c.execute('DELETE FROM cards WHERE priority=?', (priorToSearch + 1,))
+        c.execute("SELECT * FROM cards")
+        cards = c.fetchall()
+        if len(cards) > 0 and index < len(cards):
+            c.execute('DELETE FROM cards WHERE note=?', (cards[index][0],))
         conn.commit()
         conn.close()
-        self.findNew(obj)
+        if len(cards) > 0:
+            self.findNew(obj)
+        #index += 1
 
     def doItLater(self, obj):
+        global index
         self.findNew(obj)
+        index += 1
 
     def findNew(self, obj):
-        global priorToSearch
+        global index
+        print(index)
+        conn = sqlite3.connect('cards.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM cards ORDER BY priority DESC")
+        #c.execute("SELECT * FROM cards")
+        cards = c.fetchall()
+        print(cards)
+        conn.close()
+        if index >= len(cards):
+            index = 0
+
+        if len(cards) > 0:
+            self.noteLabel.text = cards[index][0]
+        """global priorToSearch
         global seenNotes
         global lastId
         cycleNumber = 0
@@ -112,7 +155,7 @@ class MyGrid(Widget):
 
         conn.commit()
         conn.close()
-        priorToSearch = priorToSearch -1
+        priorToSearch = priorToSearch -1"""
 
 
 class Window(App):
